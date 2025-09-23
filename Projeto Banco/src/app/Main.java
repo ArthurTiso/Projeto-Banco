baae6129821234ebebb0542cd8deff95cc557384
Arthur Morais <161392404+ArthurTiso@users.noreply.github.com>
2025-09-23 16:25:27 -0300

    Add files via upload

--- Projeto Banco/src/app/Main.java
+++ Projeto Banco/src/app/Main.java
@@ -9,7 +9,7 @@ import view.ContaGUI;
 import exception.SaldoInsuficienteException;
 import java.io.IOException;
 import java.util.ArrayList;
-
+import java.util.Comparator;
 
 //Main normal
 
@@ -24,21 +24,18 @@ public class Main {
         double valorSaque;
 
         try {  
-        	
+       	
         	List<ContaCorrente> contas = cs.LerContas(arquivoEntrada);
         	
-        	String dados = "Contas foram carregadas!\n\n";
+        	String dados = "Contas foram carregadas!\n\n"; //Dados carrega inicialmete a string anunciando que as contas foram carregadas, e logo em seguida (pelo uso do for) carrega também os dados de cada uma das contas (obtidos via gets)
         	for (ContaCorrente c : contas) {
-        		dados = dados
-        			+ "Conta: " + c.getNumero() + " "
-        			+ " Titular: " + c.getTitular() + " "
-        			+ " Saldo: " + c.getSaldo() + " "
-        			+"\n"; //Aqui ele faz o mesmo esquema de for para armazenar e ler os dados de ContaCorrente via a VAR c, além de puxar elas pelos get do .c(conta)
+        		dados = dados + "Conta: " + c.getNumero() + " " + " Titular: " + c.getTitular() + " " + " Saldo: " + c.getSaldo() + " " + "\n"; 
+        		//Aqui ele faz o mesmo esquema de for para armazenar e ler os dados de ContaCorrente via a VAR c, além de puxar elas pelos get do .c(conta)
         	}
         	// Mostrar em JOptionPane
             JOptionPane.showMessageDialog(null, dados, "Lista de Contas", JOptionPane.INFORMATION_MESSAGE);
             
-            //Adição de conta
+            //Adição de conta 
             int addConta = JOptionPane.showConfirmDialog(null, "Deseja adicionar uma nova conta?", "Nova conta", JOptionPane.YES_NO_OPTION);
             
             if (addConta == JOptionPane.YES_OPTION) {
@@ -59,7 +56,16 @@ public class Main {
             		cs.saveContas(contas, arquivoSaida);
             		
             		JOptionPane.showMessageDialog(null, "Conta salva com sucesso!");
-            		         		
+            		
+            		//Chama o processo de gerar as contas salvas, agora com os dados atualizados
+            		dados = "Contas foram carregadas!\n\n"; //Dados carrega inicialmete a string anunciando que as contas foram carregadas, e logo em seguida (pelo uso do for) carrega também os dados de cada uma das contas (obtidos via gets)
+                	for (ContaCorrente c : contas) {
+                		dados = dados
+                			+ "Conta: " + c.getNumero() + " "
+                			+ " Titular: " + c.getTitular() + " "
+                			+ " Saldo: " + c.getSaldo() + " "
+                			+"\n"; //Aqui ele faz o mesmo esquema de for para armazenar e ler os dados de ContaCorrente via a VAR c, além de puxar elas pelos get do .c(conta)         		
+                	}          		
                 	JOptionPane.showMessageDialog(null, dados, "Lista de Contas", JOptionPane.INFORMATION_MESSAGE); //Mostra contas novamente
                 	
             	} catch (NumberFormatException nfe) {
@@ -68,20 +74,70 @@ public class Main {
             	} catch (IOException e) {
             		JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage() + "\n Falha ao salvar conta!" );
             	}
-            	
-            	
             }
-            
-            cs.saveContas(contas, arquivoSaida); //Chamada da função lá de ContaService que salva as multiplas contas num arquivo
-
-        }catch (IOException e) {
-        	JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage());
+            	//Exclusão de contas
+            	
+            	int delConta = JOptionPane.showConfirmDialog(null, "Deseja excluir alguma conta?", "excluir conta", JOptionPane.YES_NO_OPTION);
+            	if (delConta == JOptionPane.YES_OPTION) {
+            		try {
+            			String numeroDel = JOptionPane.showInputDialog(null, "Informe o numero da conta a ser deletada: "); //String armazena numero da conta a ser deletada
+            			if (numeroDel == null) return; //Caso não entre valor, cancela a operação via return
+            			int numero = Integer.parseInt(numeroDel.trim()); //Passa valor para int e corta espaços pelo trim
+            			
+            			
+            			ContaCorrente contaRemover = null; //Declara o contaremover como nulo
+            			for (ContaCorrente c : contas ) { // For para loop que percorre todas as contas
+            				if (c.getNumero() == numero) { //Se o numero salvo p/ deletar for igual o numero pego pelo get nesse loop, o contaRemover armazena ele e quebra o loop com o BREAK
+            					contaRemover = c;
+            					break;
+            				}
+            			}
+            			
+            		if (contaRemover != null) { //Se o contaRemover for nulo, o contas.remove deleta a conta utilziando a var armazenada. O cs.savecontas salva tudo e então mostra ao usuário sucesso ou falha na operação
+            			contas.remove(contaRemover);
+            			cs.saveContas(contas, arquivoSaida);
+            			JOptionPane.showMessageDialog(null, "Conta removida com Sucerro!");
+            		} else JOptionPane.showMessageDialog(null, "Conta não encontrada");
+            		
+            		dados = "Contas foram carregadas!\n\n"; //Dados carrega inicialmete a string anunciando que as contas foram carregadas, e logo em seguida (pelo uso do for) carrega também os dados de cada uma das contas (obtidos via gets)
+                	for (ContaCorrente c : contas) {
+                		dados = dados + "Conta: " + c.getNumero() + " " + " Titular: " + c.getTitular() + " " + " Saldo: " + c.getSaldo() + " " +"\n"; 
+                		//Aqui ele faz o mesmo esquema de for para armazenar e ler os dados de ContaCorrente via a VAR c, além de puxar elas pelos get do .c(conta)         		
+                	}          		
+                	JOptionPane.showMessageDialog(null, dados, "Lista de Contas", JOptionPane.INFORMATION_MESSAGE); //Mostra contas novamente
+            		
+            		}catch (NumberFormatException nfe) {
+            			JOptionPane.showMessageDialog(null, "Erro" + nfe.getMessage() + "Digite um numero valido");
+            		} catch (IOException e) {
+            			JOptionPane.showMessageDialog(null, "Erro" + e.getMessage() + "Erro ao deletar conta");
+            		}
+            	}
+        
+        int ordenar = JOptionPane.showConfirmDialog(null, "Deseja ordenar as contas por acima de 5000?", "Ordenar", JOptionPane.YES_NO_OPTION);
+        if (ordenar == JOptionPane.YES_OPTION) { //Se usuário selecionar SIM
+        	
+        	Predicate<ContaCorrente> saldoMaior5000 = c -> c.getsaldo() > 5000;
+        	
+        	String dadosOrdenados = "Contas ordenadas por saldo: \n\n";
+        	for (ContaCorrente c : contas) {
+        		dadosOrdenados += "Conta: " +c.getNumero() + "Titular: " + c.getTitular() + "Saldo: " + c.getSaldo() + "\n\n";
+        	} //Mesmo esquema de sempre, armazena os valores em loop no dadosOrdenados enquanto pega eles via Get enquanto houverem contas
+        	JOptionPane.showMessageDialog(null, dadosOrdenados + "Lista Ordenada");
+        }   
+     
+        cs.saveContas(contas, arquivoSaida);
+        
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage());
         }
     }
 }
-/*
-//Main com GUI
 
+            		
+    
+
+//Main com GUI
+/*
 public class Main {
     public static void main(String[] args) {
         ContaService cs = new ContaService();
@@ -94,3 +150,4 @@ public class Main {
     }
 }
 */
+
--- Projeto Banco/src/service/ContaService.java
+++ Projeto Banco/src/service/ContaService.java
@@ -8,7 +8,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import java.util.ArrayList; 
-import java.util.List; 
+import java.util.List;
+
+import javax.swing.JOptionPane;
 
 import exception.SaldoInsuficienteException;
 import model.Conta;
@@ -39,6 +41,23 @@ public class ContaService {
 			}
 		}
 	
+	  //Leitura de varias contas
+    public List<ContaCorrente> LerContas(String pathArquivo) throws IOException {
+        List<ContaCorrente> contas = new ArrayList<>();//Cria arraylist para armazenar novas contas
+        try (BufferedReader br = new BufferedReader(new FileReader(pathArquivo))) { //Inicia leitura via Buffered Reader(Puxa o arquivo pela var pathArquivo
+            String linha;
+            while ((linha = br.readLine()) != null) { //Leitura vai até o fim
+                if (linha.isBlank()) continue;//Leitura continue mesmo se encontrar linha em branco
+                String[] partes = linha.split(",");//Separação via virgula
+                int numero = Integer.parseInt(partes[0].trim());//Faz parse da entrada para Int e usa o trim para remover espaço em branco!
+                String titular = partes[1].trim();
+                double saldo = Double.parseDouble(partes[2].trim());
+                contas.add(new ContaCorrente(numero, titular, saldo));
+            }
+        }
+        return contas; 
+    }
+
 	//Salva dados no txt
 	public void salvarConta(ContaCorrente conta, String pathArquivo) throws IOException {
 		try (BufferedWriter bw = new BufferedWriter(new FileWriter(pathArquivo))){
@@ -46,27 +65,14 @@ public class ContaService {
 			System.out.println("Arquivo salvo em: " + new java.io.File(pathArquivo).getAbsolutePath());
 		}
 	}
+	
+	
+	
 	//saque via conta corrente
 	 public void sacar(ContaCorrente conta, double valor) throws SaldoInsuficienteException {
 	        conta.sacar(valor);
 	}
-	 
-	 //Leitura de varias contas
-	 public List<ContaCorrente> LerContas(String pathArquivo) throws IOException {
-		 List<ContaCorrente> contas = new ArrayList<>();//Cria arraylist para armazenar novas contas
-		 try (BufferedReader br = new BufferedReader(new FileReader(pathArquivo))) { //Inicia leitura via Buffered Reader(Puxa o arquivo pela var pathArquivo
-			 String linha;
-			 while ((linha = br.readLine()) != null) { //Leitura vai até o fim
-				 if (linha.isBlank()) continue;//Leitura continue mesmo se encontrar linha em branco
-				 String[] partes = linha.split(",");//Separação via virgula
-				 int numero = Integer.parseInt(partes[0].trim());//Faz parse da entrada para Int e usa o trim para remover espaço em branco!
-				 String titular = partes[1].trim();
-				 double saldo = Double.parseDouble(partes[2].trim());
-				 contas.add(new ContaCorrente(numero, titular, saldo));
-			 }
-			}
-		 return contas; 
-}
+	 	
 	 //Para salvar múltiplas contas em um arquivo
 	 public void saveContas(List<ContaCorrente> contas, String pathArquivo) throws IOException{ //Função para escrever os dados no arquivo no caminho salvo em pathArquivo
 		 try (BufferedWriter bw = new BufferedWriter(new FileWriter(pathArquivo))) {
@@ -76,6 +82,73 @@ public class ContaService {
 			 }
 		 }
 	 }
+	 //Função para mostrar contas ao usuário
+	   public void exibirContas(String titulo, List<ContaCorrente> contas) {
+	        String dados = titulo + "\n\n"; //Dados carrega inicialmete a string anunciando que as contas foram carregadas, e logo em seguida (pelo uso do for) carrega também os dados de cada uma das contas (obtidos via gets)
+	        for (ContaCorrente c : contas) {
+	            dados = dados + "Conta: " + c.getNumero() + " " + " Titular: " + c.getTitular() + " " + " Saldo: " + c.getSaldo() + " " + "\n"; 
+	        }
+	        JOptionPane.showMessageDialog(null, dados, titulo, JOptionPane.INFORMATION_MESSAGE);
+	    }
+	 //Função para adicionar conta
+	   
+	   public void adicionarConta(List<ContaCorrente> contas, String arquivoSaida) {
+	        try {
+	            String numeroNovaC = JOptionPane.showInputDialog(null, "Numero da Conta:");
+	            if (numeroNovaC == null ) return; //Cancelar caso n entre valor
+	            int numero = Integer.parseInt(numeroNovaC.trim()); //Passa valor de String para Int e passa para numero
+
+	            String titular = JOptionPane.showInputDialog(null, "Numero do titular:");
+	            if (titular == null ) return; //Cancelar caso n entre valor
+	            
+	            String saldoNovaC = JOptionPane.showInputDialog(null, "Saldo da conta: ");
+	            if (saldoNovaC == null ) return; //Cancelar caso n entre valor
+	            double saldo = Double.parseDouble(saldoNovaC.trim()); //Passa valor de String para double e passa para saldo
+
+	            ContaCorrente novaConta = new ContaCorrente(numero, titular, saldo);
+	            contas.add(novaConta);
+	            saveContas(contas, arquivoSaida);
+
+	            JOptionPane.showMessageDialog(null, "Conta salva com sucesso!");
+
+	        } catch (NumberFormatException nfe) {
+	            JOptionPane.showMessageDialog(null, "Erro: " + nfe.getMessage() + "\n Insira caracteres válidos");
+	        } catch (IOException e) {
+	            JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage() + "\n Falha ao salvar conta!" );
+	        }
+	    }
+
+	   //Função para deletar conta
+	   
+	   public void excluirConta(List<ContaCorrente> contas, String arquivoSaida) {
+	        try {
+	            String numeroDel = JOptionPane.showInputDialog(null, "Informe o numero da conta a ser deletada: "); //String armazena numero da conta a ser deletada
+	            if (numeroDel == null) return; //Caso não entre valor, cancela a operação via return
+	            int numero = Integer.parseInt(numeroDel.trim()); //Passa valor para int e corta espaços pelo trim
+
+	            ContaCorrente contaRemover = null; //Declara o contaremover como nulo
+	            for (ContaCorrente c : contas ) { // For para loop que percorre todas as contas
+	                if (c.getNumero() == numero) { //Se o numero salvo p/ deletar for igual o numero pego pelo get nesse loop, o contaRemover armazena ele e quebra o loop com o BREAK
+	                    contaRemover = c;
+	                    break;
+	                }
+	            }
+
+	            if (contaRemover != null) { //Se o contaRemover for nulo, o contas.remove deleta a conta utilziando a var armazenada. O saveContas salva tudo e então mostra ao usuário sucesso ou falha na operação
+	                contas.remove(contaRemover);
+	                saveContas(contas, arquivoSaida);
+	                JOptionPane.showMessageDialog(null, "Conta removida com Sucesso!");
+	            } else {
+	                JOptionPane.showMessageDialog(null, "Conta não encontrada");
+	            }
+
+	        } catch (NumberFormatException nfe) {
+	            JOptionPane.showMessageDialog(null, "Erro" + nfe.getMessage() + "Digite um numero valido");
+	        } catch (IOException e) {
+	            JOptionPane.showMessageDialog(null, "Erro" + e.getMessage() + "Erro ao deletar conta");
+	        }
+	    }
+	 
 }
 
 
